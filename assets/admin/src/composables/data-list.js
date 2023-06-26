e76222e656541e5868f58ee860ed5d32f3de8423
Dev Kabir <dev.kabir01@gmail.com>
2023-06-27 01:24:24 +0600

    datatable refactored

--- assets/admin/src/composables/data-list.js
+++ assets/admin/src/composables/data-list.js
@@ -1,60 +1,74 @@
-import {ref, computed, watch, onBeforeMount, inject} from 'vue';
+import { computed, inject, onBeforeMount, ref } from 'vue';
 
 export function useDataList(props) {
+  // Reactive state variables
   const isLoading = ref(false);
   const axios = inject('axios');
   const notify = inject('notify');
-  const apiResponse = ref([]);
-  const selectedTab = ref();
+  const apiResponse = ref({});
+  const selectedTab = ref('');
 
+  // Fetch data before the component is mounted
   const fetchData = async () => {
-    if (isLoading.value) return;
+    if (isLoading.value) {
+      return;
+    }
     isLoading.value = true;
 
     try {
-      const response = await axios.get(props.endpoint);
-      apiResponse.value = response.data;
+      // Make API call to fetch data
+      const { data } = await axios.get(props.endpoint);
+      apiResponse.value = data;
+      // Set the initially selected tab to the first tab in the response
       selectedTab.value = Object.keys(apiResponse.value)[0];
     } catch (error) {
+      // Handle error
       notify.error(error.message);
     } finally {
       isLoading.value = false;
     }
   };
 
-  onBeforeMount(fetchData);
-
+  // Compute the available tabs from the API response
   const tabs = computed(() => Object.keys(apiResponse.value));
 
+  // Compute the content of the selected tab
   const selectedTabContent = computed(() => apiResponse.value[selectedTab.value]);
 
+  // Function to select a tab
   const selectTab = (tab) => {
     selectedTab.value = tab;
   };
 
-  watch(selectedTab, () => {
-    selectedTabContent.value = apiResponse.value[selectedTab.value];
-  });
-
+  // Function to clean data of a specific tab
   const clean = async (tab) => {
-    if (isLoading.value) return;
+    if (isLoading.value) {
+      return;
+    }
     isLoading.value = true;
 
     try {
+      // Make API call to clean the data of the specified tab
       await axios.get(`${props.endpoint}/clean`, {
         params: {
           file: tab,
         },
       });
+      // Display success notification and remove the cleaned tab from the response
       notify.success('Data cleaned successfully');
       delete apiResponse.value[tab];
     } catch (error) {
+      // Handle error
       notify.error(error.message);
     } finally {
       isLoading.value = false;
     }
   };
 
+  // Fetch data before the component is mounted
+  onBeforeMount(fetchData);
+
+  // Return the reactive variables and functions
   return {
     tabs,
     selectedTab,
--- assets/admin/src/composables/data-table.js
+++ assets/admin/src/composables/data-table.js
@@ -1,6 +1,4 @@
 import {computed, inject, onBeforeMount, ref} from 'vue';
-
-
 /*
  |--------------------------------------------------------------------------
  | Basic functionality
@@ -106,19 +104,30 @@ export function useDataTable(props) {
  |--------------------------------------------------------------------------
  */
 export function useSort(data) {
+  // Create a reactive object to store the sort state of each column
   const sortedColumns = ref({});
+
+  // Define a function to sort the data based on the given column
   const sortData = column => {
+    // Toggle the sort state of the column
     sortedColumns.value[column] = !sortedColumns.value[column];
+
+    // Sort the data based on the column and its sort state
     data.value.sort((a, b) => {
       let result;
+      // Compare string values using localeCompare
       if (typeof a[column] === 'string') {
         result = a[column].localeCompare(b[column]);
       } else {
+        // Compare numeric values using subtraction
         result = a[column] - b[column];
       }
+      // Reverse the result if the column is not sorted in ascending order
       return sortedColumns.value[column] ? result : -result;
     });
   };
+
+  // Return the sortData function and the sortedColumns object
   return {
     sortData, sortedColumns,
   };
@@ -130,19 +139,29 @@ export function useSort(data) {
 |--------------------------------------------------------------------------
 */
 export function useSearch(data, props) {
+  // Create a reactive object to store the search query
   const search = ref('');
+
+  // Compute the keys of the searchable columns
   const anySearchable = computed(() => props.columns.flatMap(
       obj => obj.search === true ? [obj.key] : []));
+
+  // Compute the search results based on the search query and the searchable columns
   const searchResults = computed(() => {
+    // Trim and lowercase the search query
     const query = search.value.trim().toLowerCase();
+
+    // If the query is empty, return all data
     if (!query) {
       return data.value;
     }
 
+    // Filter the data based on the search query and the searchable columns
     return data.value.filter(item => anySearchable.value.some(
         column => String(item[column]).toLowerCase().includes(query)));
   });
 
+  // Return the search object and the searchResults computed property
   return {
     search, anySearchable, searchResults,
   };
@@ -155,21 +174,33 @@ export function useSearch(data, props) {
 |--------------------------------------------------------------------------
 */
 export function usePagination(data, props) {
+  // Create a reactive object to store the current page
   let currentPage = ref(1);
+
+  // Parse the number of items per page from the props
   const per_page = parseInt(props.per_page);
+
+  // Compute the total number of pages based on the data length and items per page
   const totalPages = computed(() => {
     return Math.ceil(data.value.length / per_page);
   });
+
+  // Compute the paginated data based on the current page and items per page
   let paginatedData = computed(() => {
-    // Apply pagination based on the currentPage and itemsPerPage variables
+    // Calculate the start and end indices for the current page
     const start = (currentPage.value - 1) * per_page;
     const end = start + per_page;
+
+    // Slice the data to get only the items for the current page
     return data.value.length ? data.value.slice(start, end) : data.value;
   });
+
+  // Compute an array of page numbers for pagination controls
   let pageNumbers = computed(() => {
     return Array.from({length: totalPages.value}, (x, i) => i + 1);
   });
 
+  // Define methods to navigate between pages
   const previousPage = () => currentPage.value === 1 ?
       null :
       currentPage.value--;
@@ -177,9 +208,13 @@ export function usePagination(data, props) {
       null :
       currentPage.value++;
   const goTo = page => currentPage.value = page;
+
+  // Compute some additional properties for displaying pagination information
   let from = computed(() => (currentPage.value - 1) * per_page + 1);
   let totalResults = computed(() => data.value.length);
   let to = computed(() => Math.min(currentPage.value * per_page, totalResults.value));
+
+  // Return the state variables and methods
   return {
     from,
     to,
@@ -193,32 +228,46 @@ export function usePagination(data, props) {
     goTo,
   };
 }
-
-
 /*
 |--------------------------------------------------------------------------
 | Bulk action functionality
 |--------------------------------------------------------------------------
 */
 export function useBulkAction(data, columns) {
+  // Inject the notify function from the parent component
   const notify = inject('notify');
+
+  // Create a reactive object to store the selected rows
   const selectedRows = ref([]);
+
+  // Find the key of the column used for selecting rows
   const selectKey = columns.find(column => column.select)?.key;
+
+  // Create a reactive object to store the state of the "select all" checkbox
   const allSelected = ref(false);
+
+  // Define a method to update the state of the "select all" checkbox
   const selected = () => allSelected.value = selectedRows.value.length ===
       data.value.length;
+
+  // Define a method to select or deselect all rows
   const selectAll = () => {
     selectedRows.value = allSelected.value ?
         [...data.value.map(item => item[selectKey])] :
         [];
   };
+
+  // Define a method to delete all selected rows
   const deleteAll = () => {
     if (confirm('Are you sure??')) {
       console.log(selectedRows.value);
       notify.success('Done');
     }
   };
+
+  // Return the state variables and methods
   return {
     selected, selectKey, allSelected, selectedRows, selectAll, deleteAll,
   };
 }
+
