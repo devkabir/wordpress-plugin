2b46023e5830c8f7b06fba9673bf45c9794632ef
Dev Kabir <dev.kabir01@gmail.com>
2024-01-02 16:08:01 +0600

    ⚡️

--- install.sh
+++ install.sh
@@ -19,10 +19,12 @@ find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.json" -o -name "*.h
 find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.json" -o -name "*.html" \) -not -path "./vendor/*" -not -path "./assets/node_modules/*" -execdir sed -i "s/PluginPackage/${namespace//\//\\/}/g" {} \;
 # Search and replace "your_plugin_name" with the current directory name in all PHP, JS, JSON, and HTML files except files in the vendor folder inside src directory
 find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.json" -o -name "*.html" \) -not -path "./vendor/*" -not -path "./assets/node_modules/*" -execdir sed -i "s/your_plugin_name/${base//\//\\/}/g" {} \;
-# run composer install
-composer install
+# run composer dump-autoload
+composer dump-autoload
 # run npm install on assets dir
 npm install --prefix ./assets
 # run npm run build on assets dir
 npm run build --prefix ./assets
+# delete this file
+rm install.sh
 
